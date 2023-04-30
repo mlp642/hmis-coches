fa22181411ae6d13c6a2ce9d243ac15f43c9d112
Maria Lopez Padilla <mlp642@inlumine.ual.es>
2023-04-30 10:58:42 +0200

    add new tests

--- test/org/hmis/CocheTest.java
+++ test/org/hmis/CocheTest.java
@@ -62,5 +62,65 @@ class CocheTest {
 		Coche segundo = new Coche ("Toyota", "Corolla", 2022, 32000);
 		assertEquals(false, primero.equals(segundo));  // Action // Assert
 	}
+	
+	@Test
+	void testGetMarca() {
+		Coche coche = new Coche ("Toyota", "Corolla", 2022, 22000);
+		assertEquals(true, coche.getMarca().equals("Toyota"));  // Action // Assert
+	}
 
+	@Test
+	void testSetMarca() {
+		Coche coche = new Coche ();
+		coche.setMarca("Renault");
+		assertEquals(true, coche.getMarca().equals("Renault"));  // Action // Assert
+	}
+
+	@Test
+	void testGetModelo() {
+		Coche coche = new Coche ("Toyota", "Corolla", 2022, 22000);
+		assertEquals(true, coche.getModelo().equals("Corolla"));  // Action // Assert
+	}
+
+	@Test
+	void testSetModelo() {
+		Coche coche = new Coche ();
+		coche.setModelo("Corolla");
+		assertEquals(true, coche.getModelo().equals("Corolla"));  // Action // Assert
+	}
+	
+	@Test
+	void testGetAño() {
+		Coche coche = new Coche ("Toyota", "Corolla", 2022, 22000);
+		assertEquals(true, coche.getAño() == 2022);  // Action // Assert
+	}
+
+	@Test
+	void testSetAño() {
+		Coche coche = new Coche ();
+		coche.setAño(2000);
+		assertTrue(coche.getAño()==2000);
+		
+	}
+	
+	@Test
+	void testGetPrecio() {
+		Coche coche = new Coche ("Toyota", "Corolla", 2022, 22000);
+		assertTrue(coche.getPrecio() == 22000);  // Action // Assert
+	}
+
+	@Test
+	void testSetPrecio() {
+		Coche coche = new Coche ();
+		coche.setPrecio(24000);
+		assertEquals(true, coche.getPrecio() == 24000);   // Action // Assert
+	}
+	
+	@Test
+	void testToString() {
+		Coche coche = new Coche ("Toyota", "Corolla", 2022, 22000);
+		assertEquals("Coche [marca=Toyota, modelo=Corolla, año=2022, precio=22000]" , coche.toString());
+	
+	}
+	
 }
